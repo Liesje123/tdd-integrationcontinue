95cf256531145f27f171e231a137c1892cc94b6f
Liesje123 <elisabeth.guitton@isen-ouest.yncrea.fr>
2024-01-18 10:20:50 +0100

    Test [lire le solde d'un compte]

--- src/main/java/com/isen/project/Compte.java
+++ src/main/java/com/isen/project/Compte.java
@@ -1,11 +1,29 @@
 package com.isen.project;
 
+import java.util.List;
+import java.util.function.BooleanSupplier;
+
 public class Compte {
     private String compte;
     private String solde;
 
-    public String contenuSolde(String solde) {
-        return solde;
+    public Compte(String compte, String solde) {
+		this.compte = compte;
+		this.solde = solde;
+	}
+
+    public boolean contenuSoldeEstNull() {
+        if(solde==null){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean contenuSoldeEstNegatif() {
+        if(Integer.parseInt(solde)<=0){
+            return true;
+        }
+        return false;
     }
 
 
--- src/test/java/com/isen/project/CompteTest.java
+++ src/test/java/com/isen/project/CompteTest.java
@@ -1,14 +1,42 @@
 package com.isen.project;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Arrays;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class CompteTest {
+
+    private Compte compte;
+
+    @BeforeEach
+    void init(){
+        compte = new Compte("15","1500");
+    }
+
+    /*
+     * TEST SOLDE
+     */
     
+    /*
+     * Est-ce que le solde d'un compte est null 
+     */
     @Test
-    void contenuSoldeNonNull(){
-        Compte compte = new Compte();
-        assertNotNull(compte.contenuSolde("15000"));
+    @DisplayName("Test - compte - Est-ce que le solde d'un compte est null ")
+    void contenuSoldeNull(){
+        assertFalse(compte.contenuSoldeEstNull());
+    }
+
+    /*
+     * Est-ce que le solde d'un compte est négatif 
+     */
+    @DisplayName("Test - compte - Est-ce que le solde d'un compte est négatif ")
+    @Test
+    void contenuSoldeNegatif(){
+        assertFalse(compte.contenuSoldeEstNegatif());
     }
 }
